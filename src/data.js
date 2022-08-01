681e31fda6d8e01ac2181da7ed7ace53e76d051a
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-08-02 00:01:57 +0530

    added products

--- src/data.js
+++ src/data.js
@@ -52,7 +52,7 @@ export const popularProducts = [
   },
   {
     id: 2,
-    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    img: "https://m.media-amazon.com/images/I/31e+y1dY05L._AC_UL480_FMwebp_QL65_.jpg",
   },
   {
     id: 3,
@@ -72,7 +72,7 @@ export const popularProducts = [
   },
   {
     id: 7,
-    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    img: "https://m.media-amazon.com/images/I/51YQ9BhqHTL._AC_UL480_FMwebp_QL65_.jpg",
   },
   {
     id: 8,
