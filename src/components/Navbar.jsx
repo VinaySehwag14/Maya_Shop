6aa2997d2537b5883580d7b79eaf790bebc96e3b
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-08-05 11:37:04 +0530

    added single product page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,35 @@
+// import { Badge } from "@material-ui/core";
+// import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Search, ShoppingCartOutlined } from "@mui/icons-material";
+import { Badge } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
-import { Search, ShoppingCartOutlined } from "@mui/icons-material";
-
-import Badge from "@mui/material/Badge";
-
-//* styled components
+// import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
 `;
+
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
   align-items: center;
 `;
 
-const Language = styled.div`
+const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
 `;
 
 const SearchContainer = styled.div`
-  border: 0.5px solid lightgrey;
+  border: 0.5px solid lightgray;
   display: flex;
   align-items: center;
   margin-left: 25px;
@@ -33,18 +40,13 @@ const Input = styled.input`
   border: none;
 `;
 
-const Logo = styled.h1`
-  font-weight: bold;
-`;
-
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-`;
 const Center = styled.div`
   flex: 1;
   text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 const Right = styled.div`
   flex: 1;
@@ -66,18 +68,19 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input /> <Search style={{ color: "gray", fontSize: "16px" }} />
+            <Input placeholder="Search" />
+            <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>MAYA SHOP</Logo>
+          <Logo>MAYA.</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
           <MenuItem>SIGN IN</MenuItem>
           <MenuItem>
             <Badge badgeContent={4} color="primary">
-              <ShoppingCartOutlined color="action" />
+              <ShoppingCartOutlined />
             </Badge>
           </MenuItem>
         </Right>
