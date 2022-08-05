d24ab5f4c824db04d221aad2ae72079c36dccebf
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-08-05 11:45:41 +0530

    added login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
 
-import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+// import Product from "./pages/Product";
 
 const App = () => {
-  return <Product />;
+  return <Register />;
 };
 
 export default App;
