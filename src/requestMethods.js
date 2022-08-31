adea9958784f9a4dc55e92d77b50bf8b884eb594
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-08-31 14:35:10 +0530

    added dynamic token mehtod in request methods

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZjIzZTcxMmJkNDRiMGZkYjcyY2Q3ZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MDg5NDY2OSwiZXhwIjoxNjYxMTUzODY5fQ.JCsTEV2LEeriHldmiQWH1o5GA9-9gjgTmqWW7bhtWHo";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+  .currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
