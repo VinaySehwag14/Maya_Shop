be6ba384fa8b8cc0afa3b378420c03c276690bf6
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-04-19 14:24:59 +0530

    code updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -68,23 +68,40 @@ const Icon = styled.div`
 
 const Product = ({ item }) => {
   return (
-    <Container>
-      <Circle />
-      <Image src={item.img} />
-      <Info>
-        <Icon>
+    <div
+      className="flex-1 m-[5px] min-w-[280px] h-[350px] flex items-center
+    justify-center bg-[#f5fbfd] relative group cursor-pointer  "
+    >
+      <div className="w-[200px] h-[200px] rounded-full bg-white absolute "></div>
+
+      <img className="h-3/4 z-[2]" src={item.img} alt="clothes" />
+      <div
+        className="w-full h-full absolute top-0 left-0 bg-[rgba(0,0,0,0.2)] z-[3]
+        flex items-center justify-center opacity-0 group-hover:opacity-100 transition
+        duration-500 ease-in-out "
+      >
+        <div
+          className="w-[40px] h-[40px] rounded-full bg-white flex items-center justify-center
+          m-[10px] hover:bg-[#e9f5f5] transform hover:scale-110 transition duration-500 cursor-pointer "
+        >
           <ShoppingCartOutlined />
-        </Icon>
-        <Icon>
+        </div>
+        <div
+          className="w-[40px] h-[40px] rounded-full bg-white flex items-center justify-center
+          m-[10px] hover:bg-[#e9f5f5] transform hover:scale-110 transition duration-500 cursor-pointer "
+        >
           <Link to={`/product/${item._id}`}>
             <SearchOutlined />
           </Link>
-        </Icon>
-        <Icon>
+        </div>
+        <div
+          className="w-[40px] h-[40px] rounded-full bg-white flex items-center justify-center
+          m-[10px] hover:bg-[#e9f5f5] transform hover:scale-110 transition duration-500 cursor-pointer "
+        >
           <FavoriteBorderOutlined />
-        </Icon>
-      </Info>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 };
 
